b369ccb3e18ccfd1b3cef9b1ec8d2073c0bfb1f8
erichaney <ehaney@outlook.com>
2021-12-03 21:36:31 -0700

    Add menu bar and containers for scene

--- src/main/java/com/ehaney/fitch/FitchApp.java
+++ src/main/java/com/ehaney/fitch/FitchApp.java
@@ -1,8 +1,9 @@
 package com.ehaney.fitch;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -12,10 +13,22 @@ public class FitchApp extends Application {
     @Override
     public void start(Stage stage) {
         BorderPane root = new BorderPane();
+        root.setTop(initMenuBar());
+        root.setCenter(initEditor());
         Scene scene = new Scene(root);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private SplitPane initEditor() {
+        // TODO
+        return new SplitPane();
+    }
+
+    private MenuBar initMenuBar() {
+        // TODO
+        return new MenuBar();
     }
 
     public static void main(String[] args) {
